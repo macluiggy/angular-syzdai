32be185666833b6ca73b83f408b818ce381cfd7d
luiggy macias <ferrinluiggy@gmail.com>
2022-02-26 17:01:03 -0500

    learning the basics

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,20 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { products } from '../products';
+import { products } from "../products";
 
 @Component({
-  selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  selector: "app-product-list",
+  templateUrl: "./product-list.component.html",
+  styleUrls: ["./product-list.component.css"],
 })
 export class ProductListComponent {
   products = products;
-
-  share() {
-    window.alert('The product has been shared!');
+  x: any;
+  share(x: any) {
+    x = x;
+    if (x) return window.alert(`The product has been shared! ${x}`);
+    return window.alert(`The product has been shared! ${x}`);
   }
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
