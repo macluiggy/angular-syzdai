225a8c05196938d44fafb4b25b5529fef42dbd7f
luiggy macias <ferrinluiggy@gmail.com>
2022-03-05 16:47:46 -0500

    adding service class and practicing routing

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -18,6 +18,7 @@ export class ProductDetailsComponent implements OnInit {
 
   addToCart(product: Product) {
     this.cartService.addToCart(product);
+    window.alert("Your product has been added to the cart!");
   }
 
   ngOnInit(): void {
